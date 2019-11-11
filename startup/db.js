8e1ce1fb9596e5ae240bbc29a1a4adc06ded3b7e
wgeorge1990 <williameverettgeorge@icloud.com>
2019-11-10 19:22:36 -0500

    still down on heroku, research needed for solution

--- startup/db.js
+++ startup/db.js
@@ -5,6 +5,6 @@ const config = require('config');
 module.exports = function() {
     // Connect to mongoDB
     // Runs based on enviornment set at NODE_ENV= ( test, development, production )config.get('db')
-    mongoose.connect("mongodb+srv://videoNestUser:1234@cluster0-kvrje.mongodb.net/test?retryWrites=true&w=majority",{ useFindAndModify: false, useUnifiedTopology: true ,useNewUrlParser: true }) // Line to connect to mongoDB.Compass
+    mongoose.connect(config.get('db'),{ useFindAndModify: false, useUnifiedTopology: true ,useNewUrlParser: true }) // Line to connect to mongoDB.Compass
             .then(() => winston.info(`Connected to ${config.get('db')} ===> 😱`));
 };
