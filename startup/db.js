66a81d112035bf183d5ef3a9df47a38067ead4ed
wgeorge1990 <williameverettgeorge@icloud.com>
2019-11-10 19:14:30 -0500

    env correction for mongoDB network error

--- startup/db.js
+++ startup/db.js
@@ -4,11 +4,7 @@ const config = require('config');
 
 module.exports = function() {
     // Connect to mongoDB
-    // Runs based on enviornment set at NODE_ENV= ( test, development, production )
-    mongoose.connect(config.get('db'),
-        { useFindAndModify: false,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        }) // Line to connect to mongoDB.Compass
+    // Runs based on enviornment set at NODE_ENV= ( test, development, production )config.get('db')
+    mongoose.connect("mongodb+srv://videoNestUser:1234@cluster0-kvrje.mongodb.net/test?retryWrites=true&w=majority",{ useFindAndModify: false, useUnifiedTopology: true ,useNewUrlParser: true }) // Line to connect to mongoDB.Compass
             .then(() => winston.info(`Connected to ${config.get('db')} ===> 😱`));
 };
